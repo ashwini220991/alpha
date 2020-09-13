e01fe31748519c80e81182f3dba62343b31b2375
ashwini <ashwini05rahul@gmail.com>
2020-09-13 21:41:58 +0530

    new code

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
 	has_many :author_books
 	has_many :authors, through: :author_books
+	 validates :book_title, presence: true, length: { minimum: 6, maximum: 100 }
 
 end
